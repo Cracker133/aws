a944a3fc1adff858f0f1aefa7e191266937c4166
Vamsi <86515605+Cracker133@users.noreply.github.com>
2021-09-30 08:33:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask
-# from flask.wrappers import Request
-from flasgger import Swagger
-import os
+
 
 # WSGI
 app = Flask(__name__)
-Swagger(app)
+
 
 @app.route('/')
 def welcome():
@@ -13,6 +11,5 @@ def welcome():
 
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT',5000))
-    app.run(host= '0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run()
